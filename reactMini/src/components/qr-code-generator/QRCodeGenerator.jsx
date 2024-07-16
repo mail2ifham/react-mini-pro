0e271ef76db3f7c83f6559abb654c30e57fa04ae
ifham <mail2ifham@gmail.com>
2024-07-16 13:37:29 +0530

    done with Qr code

--- reactMini/src/components/qr-code-generator/QRCodeGenerator.jsx
+++ reactMini/src/components/qr-code-generator/QRCodeGenerator.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import './style.css'
+import QRCode from 'react-qr-code'
 
-function QRCodeGenerator(){
-return<>
-<h1>salam</h1>
-</>
+function QRCodeGenerator() {
+    const [qrCode,setQrCode] = useState('');
+    const [value, setValue] = useState('')
+
+    function handleClick(){
+        setQrCode(value)
+        setValue('')
+    }
+
+    return <div style={{ textAlign: "center" }}>
+        <h1>QR Code Generator</h1>
+        <div>
+            <input type="text " name='qr-code' placeholder='Enter your value here' 
+            onChange={e=>setValue(e.target.value)} value={value}
+            />
+            <button onClick={handleClick} > Generate</button>
+        </div>
+        <div style={{ background: 'white', padding: '16px' }}>
+            <QRCode id='qr-code' size={400} value={qrCode}/>
+        </div>
+    </div>
 }
 
 export default QRCodeGenerator;
